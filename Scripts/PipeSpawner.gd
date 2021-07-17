0d453fadc4ab96ac3add8bafe97ba7b7483bea22
Emerson Costa <emerson.costa2785@gmail.com>
2021-07-17 19:36:30 -0300

    Criação do Pipespawner e do script para instanciar os canos

---
+++ Scripts/PipeSpawner.gd
@@ -0,0 +1,15 @@
+extends Position2D
+
+# Aqui irá carregar uma Cena, pode ser utilizado tanto para cena quanto para uma imagem.
+const PIPE = preload("res://Scenes/Pipe.tscn")
+
+func spaw_pipe():
+	var pipe_instanced = PIPE.instance()
+	randomize() # Não deixa repetir os numeros aleatórios
+	var random_pipe_position = rand_range(-90, 45)
+	pipe_instanced.position  = Vector2(0,random_pipe_position) 
+	add_child(pipe_instanced)
+
+# Foi inserirdo na cena um objeto chamado Time, depois selecionou o Timer e foi no guia Nó e add a função timeout()
+func _on_Timer_timeout():
+	spaw_pipe()
